c195f87aca68729126343f0606e04e3bc1ddef1b
Cristina_Voitic <kristinavoytik@gmail.com>
2022-12-14 20:23:23 +0300

    2

--- download_with_requests.py
+++ download_with_requests.py
@@ -1,4 +1,6 @@
 import requests
+import os
+
 
 url = 'https://www.selenium.dev/images/sponsors/browserstack.png'
 response = requests.get(url=url)
@@ -7,6 +9,6 @@ content = response.content
 with open('selenium_image.png', 'wb') as file:
     file.write(content)
 
-import os
+
 size = os.path.getsize('selenium_image.png')
 assert size == 23783
--- download_with_selene.py
+++ download_with_selene.py
@@ -2,6 +2,8 @@ import time
 
 from selene.support.shared import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 prefs = {
@@ -10,7 +12,10 @@ prefs = {
 }
 options.add_experimental_option('prefs', prefs)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+browser.config.driver = driver
 
 browser.open('https://demoqa.com/upload-download')
 browser.element('#downloadButton').click()
 time.sleep(1)
+
+assert os.path.getsize('tmp/sampleFile.jpeg') == 4096
